130a805daedecd26568b40ac61532fc6486cc986
echeadle <echeadle@gmail.com>
2023-12-15 14:13:32 -0700

    end of lecture 4

--- controls.js
+++ controls.js
@@ -1,46 +1,45 @@
-class Controls {
-    constructor() {
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+class Controls{
+    constructor(){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.#addKeyboradListeners();
+        this.#addKeyboardListeners();
     }
 
-    #addKeyboradListeners() {
-        document.onkeydown = (event) => {
-            switch (event.key) {
-                case "ArrowUp":
-                    this.forward = true;
-                    break;
-                case "ArrowDown":
-                    this.reverse = true;
-                    break;
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = true;
+                    this.right=true;
                     break;
-            }
-            console.table(this);
-        };
-        document.onkeyup = (event) => {
-            switch (event.key) {
                 case "ArrowUp":
-                    this.forward = false;
+                    this.forward=true;
                     break;
                 case "ArrowDown":
-                    this.reverse = false;
-                    break;
-                case "ArrowLeft":
-                    this.left = false;
-                    break;
-                case "ArrowRight":
-                    this.right = false;
+                    this.reverse=true;
                     break;
             }
-        };
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+           }
+        }
     }
 }
