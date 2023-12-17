1baffd755053105fde0bfa367d81e53d8049b885
echeadle <echeadle@gmail.com>
2023-12-16 19:38:33 -0700

    End of Lecture 6

--- car.js
+++ car.js
@@ -4,7 +4,7 @@ class Car{
         this.y=y;
         this.width=width;
         this.height=height;
-
+        
         this.speed=0;
         this.acceleration=0.2;
         this.maxSpeed=maxSpeed;
@@ -12,8 +12,13 @@ class Car{
         this.angle=0;
         this.damaged=false;
 
+        this.useBrain=controlType=="AI";
+
         if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
     }
@@ -26,6 +31,18 @@ class Car{
         }
         if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+            console.log(outputs);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
     }
 
