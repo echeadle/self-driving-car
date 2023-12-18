8b472ebfec5e5b5b5a05d5b361a7ccb33ce1574e
echeadle <echeadle@gmail.com>
2023-12-18 10:56:35 -0700

    End of Lecture 7

--- visualizer.js
+++ visualizer.js
@@ -6,13 +6,31 @@ class Visualizer{
         const width=ctx.canvas.width-margin*2;
         const height=ctx.canvas.height-margin*2;
 
-        Visualizer.drawLevel(ctx,network.levels[0],
-            left,top,
-            width,height
-        );
+        const levelHeight=height/network.levels.length;
+
+        for(let i=network.levels.length-1;i>=0;i--){
+            const levelTop=top+
+                lerp(
+                    height-levelHeight,
+                    0,
+                    network.levels.length==1
+                        ?0.5
+                        :i/(network.levels.length-1)
+                );
+            
+            ctx.setLineDash([7,3]);
+            Visualizer.drawLevel(ctx,network.levels[i],
+                left,levelTop,
+                width,levelHeight,
+                i==network.levels.length-1
+                    ?['\u2191', '\u2190','\u2192','\u2193']
+                    :[]
+            );  
+        }
+        
     }
 
-    static drawLevel(ctx,level,left,top,width,height){
+    static drawLevel(ctx,level,left,top,width,height,outputLabels){
         const right=left+width;
         const bottom=top+height;
 
@@ -61,13 +79,24 @@ class Visualizer{
 
             ctx.beginPath();
             ctx.lineWidth=2;
-            ctx.arc(x,top,nodeRadius,0,Math.PI*2);
+            ctx.arc(x,top,nodeRadius*0.8,0,Math.PI*2);
             ctx.strokeStyle=getRGBA(biases[i]);
             ctx.setLineDash([3,3]);
             ctx.stroke();
             ctx.setLineDash([]);
+        
+            if(outputLabels[i]){
+                ctx.beginPath();
+                ctx.textAlign="center";
+                ctx.textBaseline="middle";
+                ctx.fillStyle="black";
+                ctx.strokeStyle="white";
+                ctx.font=(nodeRadius*1.5)+"px Arial";
+                ctx.fillText(outputLabels[i],x,top-nodeRadius*0.15);
+                ctx.lineWidth=0.5;
+                ctx.strokeText(outputLabels[i],x,top-nodeRadius*0.);
+            }
         }
-
     }
 
     static #getNodeX(nodes,index,left,right){
