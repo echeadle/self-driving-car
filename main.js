255352900a1117f5631e1b529b07a843358676a7
echeadle <echeadle@gmail.com>
2023-12-31 16:12:57 -0700

    Add the worled builder from self-driving-car_2

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const carCanvas=document.getElementById("carCanvas");
-carCanvas.width=200;
+carCanvas.width=window.innerWidth -330;
 const networkCanvas=document.getElementById("networkCanvas");
 networkCanvas.width=300;
 
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
-const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 const N=1;
 const cars=generateCars(N);
@@ -21,15 +20,8 @@ if(localStorage.getItem("bestBrain")){
     }
 }
 
-const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
-];
+const traffic = [];
+const roadBoarders = [];
 
 animate();
 
@@ -46,17 +38,17 @@ function discard(){
 function generateCars(N){
     const cars=[];
     for(let i=0;i<N;i++){
-        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"));
+        cars.push(new Car(100, 100,30,50,"KEYS"));
     }
     return cars;
 }
 
 function animate(time){
     for(let i=0;i<traffic.length;i++){
-        traffic[i].update(road.borders,[]);
+        traffic[i].update(roadBorders,[]);
     }
     for(let i=0;i<cars.length;i++){
-        cars[i].update(road.borders,traffic);
+        cars[i].update(roadBorders,traffic);
     }
     bestCar=cars.find(
         c=>c.y==Math.min(
@@ -70,7 +62,7 @@ function animate(time){
     carCtx.save();
     carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
 
-    road.draw(carCtx); 
+  
     for(let i=0;i<traffic.length;i++){
         traffic[i].draw(carCtx,"red");
     }
